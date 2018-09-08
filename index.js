dbc2992fba84d5115dad98442dcd1d11a30834c2
paorendor@gmail.com rendor <paorendor@gmail.com>
2018-09-08 22:01:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidaysTo(name)
 
 function happyHolidayTo(holiday, name)
 {
-  console.log `Happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday,number)
